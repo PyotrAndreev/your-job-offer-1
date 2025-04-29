b13641276b57c3874783137637d42a77cdb9050b
Duhinvalt <shirokoriad.ao@phystech.edu>
2025-04-29 13:51:38 +0300

    Add files via upload

---
+++ web-app/backend/hhru_api.py
@@ -0,0 +1,116 @@
+import requests
+import json
+
+client_id = "GES2OLI3SIBO9IEP71CQNM9P35M6FRG29SGD1JFICCRI2P2PQD6F5SBFQHLDO3LD"
+client_secret = "NV4JTC1JU0IJNQLBEQLSDDSOQ2RSMDFGNA3NI8UNAJT2R7IGUVIPVNELQU5DR8FG"
+
+def exchange_code_for_tokens(authorization_code):
+	global client_id, client_secret
+
+	data = {
+		"grant_type": "authorization_code",
+		"client_id": client_id,
+		"client_secret": client_secret,
+		"code": authorization_code,
+		"redirect_uri" : "https://github.com/yn864/your-job-offer.git"
+	}
+
+	headers = {
+		"HH-User-Agent": "YourJobOffer_1 (shirokoriad.ao@phystech.edu)",
+		"Content-Type": "application/x-www-form-urlencoded",
+	}
+
+	response = requests.post("https://api.hh.ru/token", data=data, headers=headers)
+
+	if response.status_code == 200:
+		tokens = response.json()
+		access_token = tokens.get("access_token")
+		refresh_token = tokens.get("refresh_token")
+	else:
+		access_token = None
+		refresh_token = None
+	return (access_token, refresh_token)
+
+
+def exchange_refresh_token_for_tokens(refresh_token):
+	global client_id, client_secret
+
+	data = {
+		"grant_type": "refresh_token",
+		"client_id": client_id,
+		"client_secret": client_secret,
+		"refresh_token": refresh_token,
+		"redirect_uri" : "https://github.com/yn864/your-job-offer.git"
+	}
+
+	headers = {
+		"HH-User-Agent": "YourJobOffer_1 (shirokoriad.ao@phystech.edu)",
+		"Content-Type": "application/x-www-form-urlencoded",
+	}
+
+	response = requests.post("https://api.hh.ru/token", data=data, headers=headers)
+
+	if response.status_code == 200:
+		tokens = response.json()
+		access_token = tokens.get("access_token")
+		refresh_token = tokens.get("refresh_token")
+	else:
+		access_token = None
+		refresh_token = None
+	return (access_token, refresh_token)
+
+
+# message - сопроводительное письмо не более 10000 символов
+def apply_for_vacancy(access_token, resume_id, vacancy_id, message):
+	headers = {
+		"HH-User-Agent": "YourJobOffer_1 (shirokoriad.ao@phystech.edu)",
+		"Authorization": f"Bearer {access_token}",
+	}
+
+	data = {
+		"resume_id": resume_id,
+		"vacancy_id": vacancy_id,
+		"message": message,
+	}
+
+	res = requests.post("https://api.hh.ru/negotiations", data=data, headers=headers)
+
+	if res.status_code == 201:
+		print(f"Отклик на вакансию c vacancy_id {vacancy_id} по resume_id {resume_id} совершен")
+		return True
+	elif res.status_code == 303:
+		location = res.headers.get("Location")
+		if location:
+			print("Перейдите на сайт работодателя: " + location)
+		else:
+			print("Схема отклика без внешнего URL")
+		return False
+	elif res.status_code == 400:
+		print(res.content)
+		print("Ошибка в параметрах запроса")
+		return False
+	elif res.status_code == 403:
+		print("Невозможно откликнуться на вакансию")
+		return False
+	else:
+		print("Неизвестная ошибка при отклике")
+		return False
+
+
+def get_users_last_resume_id(access_token):
+	headers = {
+		"HH-User-Agent": "YourJobOffer_1 (shirokoriad.ao@phystech.edu)",
+		"Authorization": f"Bearer {access_token}",
+		"Content-Type": "application/json",
+	}
+
+	res = requests.get("https://api.hh.ru/resumes/mine", headers=headers)
+
+	if res.status_code == 200:
+		data = res.json()
+		if data['items']:
+			 # Первое возвращаемое - это последнее созданное
+			first_resume_id = data['items'][0]['id']
+			return(first_resume_id)
+	else:
+		return None
---
+++ web-app/backend/updating_bd.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import sessionmaker
+from models import Updates, engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def add_agregator(agregator_name):
+    session.add(Updates(agregator=agregator_name))
+    session.commit()
+
+add_agregator("head hunter") ## айдишник hh - 1
+add_agregator("habr career") ## айдишник hc - 2
+add_agregator("superjob") ## айдишник sj - 3
+add_agregator("zarplate") ## айдишник zp - 4
+
+def update_time(agregator_id, time):
+    update_info = session.query(Updates).filter_by(update_id=agregator_id).first()
+    if update_info:
+        update_info.updated_at = time
+        session.commit()
+
+def get_last_update(agregator_id):
+    update_info = session.query(Updates).filter_by(update_id=agregator_id).first()
+    if update_info:
+        return update_info.updated_at
